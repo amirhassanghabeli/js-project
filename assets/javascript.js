2994e8b5a40f0516b294f589f003c4ac7e5e517d
amirhassan <amirhassanghabeli@gmail.com>
2024-08-01 17:26:01 -0700

    js-project

---
+++ assets/javascript.js
@@ -0,0 +1,75 @@
+
+
+
+
+// var num = 35
+// if (num % 2 == 0) {
+//     console.log("زوج")
+// }
+// else {
+//     console.log("فرد")
+// }
+
+// var num = 4
+// if (num % 2 == 0) {
+//     console.log("zoj")
+// }
+// else {
+//     console.log("fard")
+// }
+
+
+
+
+
+// var a = 5
+// console.log(a)
+// var b = 10
+// console.log(a + b)
+
+
+
+
+// var num = 5
+// if (num % 5 == 0) {
+//     console.log("yes")
+// }
+// else {
+//     console.log("no")
+// }
+// var num = 23
+// if (num % 5 == 0) {
+//     console.log("yes")
+// }
+// else {
+//     console.log("no")
+// }
+
+
+
+// var a = 10
+// a = 50
+// console.log(a)
+
+
+// var vallet = 10
+// var theProduct = 2
+// var price = 6
+
+// if (vallet >= price && theProduct > 0) {
+//     console.log("قابل خرید")
+// }
+
+// var vallet = 10
+// var theProduct = 2
+// var price = 6
+// if (theProduct > 0 || vallet >= price) {
+//     console.log("مشاهده کالا")
+// }
+
+// var a="amir"
+// if(a.length<5){
+//     console.log("مقدار وارد شده صحیح نیست"
+
+
+
